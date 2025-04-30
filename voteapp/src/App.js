218ada783ff409d4c1a7b64d2646193e6352cb23
sozerong <dudfo0893@naver.com>
2025-04-30 16:34:06 +0900

    배포전단계

--- voteapp/src/App.js
+++ voteapp/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const GITHUB_IMAGE_BASE = "https://raw.githubusercontent.com/sozerong/VotePoster/main";
 
 function App() {
